b4039505eea0c317737c11fa168bcd469012b533
swandevson <shai4956@gmail.com>
2022-12-03 03:39:14 +0900

    Implement(MainController): 모든 controller에 대한 기능 추가

--- src/main/java/Controller/MainController.java
+++ src/main/java/Controller/MainController.java
@@ -16,19 +16,34 @@ import view.OutputView;
 
 public class MainController {
     final private OrderController orderController = new OrderController();
+    final private PayController payController = new PayController();
     final private TableOrders tableOrders = new TableOrders();
 
 
     public void run() {
-        final int commandNumber = InputView.inputCommandNumber();
-        InputValidator.validateCommandNumber(commandNumber);
+        while (true) {
+            final int commandNumber = InputView.inputCommandNumber();
+            InputValidator.validateCommandNumber(commandNumber);
 
-        if (Command.ORDER.equals(commandNumber)) {
-            order();
+            if (Command.ORDER.equals(commandNumber)) {
+                order();
+            }
+
+            if (Command.PAY.equals(commandNumber)) {
+                pay();
+            }
+
+            if (Command.QUIT.equals(commandNumber)) {
+                return;
+            }
         }
     }
 
     private void order() {
         orderController.process(tableOrders);
+    }
+
+    private void pay() {
+        payController.process(tableOrders);
     }
 }
