a7f82d2140c3aee4b8b194b31b2c1c0237fefac3
swandevson <shai4956@gmail.com>
2022-12-03 00:00:28 +0900

    Refactor(MainController): Application의 작동을 MainController로 이동

--- src/main/java/Controller/MainController.java
+++ src/main/java/Controller/MainController.java
@@ -1,4 +1,23 @@
 package Controller;
 
+import domain.Menu;
+import domain.MenuRepository;
+import domain.Table;
+import domain.TableRepository;
+import java.util.List;
+import view.InputView;
+import view.OutputView;
+
+
+
 public class MainController {
+    public void run() {
+        final List<Table> tables = TableRepository.tables();
+        OutputView.printTables(tables);
+
+        final int tableNumber = InputView.inputTableNumber();
+
+        final List<Menu> menus = MenuRepository.menus();
+        OutputView.printMenus(menus);
+    }
 }
