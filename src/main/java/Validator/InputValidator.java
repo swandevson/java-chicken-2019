d8a74147adda7a752e90ce73b827bd941d1a3edf
swandevson <shai4956@gmail.com>
2022-12-03 00:49:34 +0900

    Feat(InputValidator): 메뉴 개수 검증

--- src/main/java/Validator/InputValidator.java
+++ src/main/java/Validator/InputValidator.java
@@ -9,6 +9,9 @@ import java.util.Arrays;
 
 
 public class InputValidator {
+    private static final int MAX_MENU_COUNT = 99;
+
+
     public static void validateCommandNumber(int number) throws IllegalArgumentException {
         for (Command command : Command.values()) {
             if (command.equals(number)) {
@@ -28,6 +31,16 @@ public class InputValidator {
     public static void validateMenuNumber(int number) throws IllegalArgumentException {
         if (!MenuRepository.isExistMenu(number)) {
             throw new IllegalArgumentException("존재하지 않는 메뉴입니다.");
+        }
+    }
+
+    public static void validateMenuCount(int count) throws IllegalArgumentException {
+        if (count <= 0) {
+            throw new IllegalArgumentException("최소 1개는 주문해야합니다.");
+        }
+
+        if (count > MAX_MENU_COUNT) {
+            throw new IllegalArgumentException("너무 많은 주문량입니다");
         }
     }
 }
